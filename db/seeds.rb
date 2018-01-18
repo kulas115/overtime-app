b7f1e8ae4e862c5ecd4c3938e5cc3f9ef96dc59f
Kulas115 <kulas115@gmail.com>
2018-01-18 19:53:00 +0100

    Implemented edit functionality for status

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,11 @@
 
 puts "1 user created"
 
+AdminUser.create(email: "admin@test.com", password: "foobar", password_confirmation: "foobar", first_name: "Admin", last_name: "User")
+
+puts "1 admin created"
+
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
