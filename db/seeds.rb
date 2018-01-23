a0c7573a2a05b779526908a69470a8e80514d053
Kulas115 <kulas115@gmail.com>
2018-01-23 21:47:49 +0100

    Brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ puts "1 admin created"
 
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
