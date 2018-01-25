01eb7fbbe6c4ad39b2b5ec790e815bae5c1afaca
Kulas115 <kulas115@gmail.com>
2018-01-25 22:20:07 +0100

    Updated seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,15 +16,14 @@ AdminUser.create(email: "admin@test.com",
 
 puts "1 admin created"
 
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 audit_logs have been created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
-
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-
-puts "3 audit_logs have been created"
